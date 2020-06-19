880832c61f59977b84dbbc27b838171a7f634266
ProfessorChristian <51305140+ProfessorChristian@users.noreply.github.com>
2020-06-19 15:57:09 +0800

    DaveCommit_MobileController_Model

---
+++ SOPCOVIDChecker/Models/MobileModels/MainUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOPCOVIDChecker.Models.MobileModels
+{
+    public class MainUserModel
+    {
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string UserLevel { get; set; }
+        public string Facility { get; set; }
+        public int DiseaseReportingUnitId { get; set; }
+    }
+}
